ab48c242c79ce958ccb6508ceb8f55a2d5b0d9fc
Haisham <47662901+m-haisham@users.noreply.github.com>
2024-09-16 17:45:20 +0500

    fix: escape variables in Nginx configuration for proper parsing

--- setup.sh
+++ setup.sh
@@ -278,7 +278,7 @@ server {
     charset utf-8;
  
     location / {
-        try_files $uri $uri/ /index.php?$query_string;
+        try_files \$uri \$uri/ /index.php?\$query_string;
     }
  
     location = /favicon.ico { access_log off; log_not_found off; }
@@ -288,7 +288,7 @@ server {
  
     location ~ \.php$ {
         fastcgi_pass unix:/var/run/php/php8.3-fpm.sock;
-        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
+        fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;
         include fastcgi_params;
         fastcgi_hide_header X-Powered-By;
     }
